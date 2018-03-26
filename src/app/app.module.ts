dd64bf520133c51de70ba5056f2dc61804717360
unknown <daniel.ungersboeck@gmail.com>
2018-03-26 13:47:13 +0200

    init components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductTableComponent } from './product-table/product-table.component';
+import { ProductInputComponent } from './product-input/product-input.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductTableComponent,
+    ProductInputComponent
   ],
   imports: [
     BrowserModule
